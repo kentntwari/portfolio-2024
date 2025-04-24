61cd6e8fc355392f6e3ae68b4142bdb746cf42ab
Kent Ntwari <designerjehovah@gmail.com>
2025-04-24 19:08:20 -0400

    chore: induce maintenance mode

--- app/root.tsx
+++ app/root.tsx
@@ -79,28 +79,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body className="py-8 bg-site font-body font-normal text-base sm:text-lg text-light">
         <div className="w-full m-auto min-h-[100dvh] min-h-screen flex flex-col">
-          <nav className="w-full max-w-[22.875rem] sm:container sm:px-5 md:px-10 lg:px-16 xl:px-20 2xl:px-[7.5rem] m-auto flex items-start justify-between">
-            <span>Kent Ntwari</span>
-            <div className="flex items-center gap-20">
-              <ArrowLink to="#contact">Contact</ArrowLink>
-              <Link to="https://github.com/kentntwari">
-                <Github className="w-6" />
-              </Link>
-            </div>
-          </nav>
           <main className="md:container flex-1 mt-24">{children}</main>
-          <footer className="mt-36 sm:mt-60 mx-auto sm:px-5 md:px-10 lg:px-16 xl:px-20 2xl:px-[7.5rem] w-full max-w-[22.875rem] sm:container sm:flex sm:flex-row-reverse sm:items-center sm:justify-between space-y-6 sm:space-y-0">
-            <div className="flex flex-col sm:flex-row gap-3 sm:gap-20">
-              <ArrowLink to="https://www.linkedin.com/in/kentntwari/">
-                Linkedin
-              </ArrowLink>
-              <ArrowLink to="https://github.com/kentntwari">Github</ArrowLink>
-            </div>
-            <span className="flex items-center gap-2">
-              <Copyright className="w-5" />
-              Kent Ntwari. 2024
-            </span>
-          </footer>
+=
         </div>
 
         <ScrollRestoration />
